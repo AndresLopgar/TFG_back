e16e9e0e9de9572b91ad31dd2b40861680f10463
andres jesús lópez garcía <andreslopgar17@gmail.com>
2024-05-10 11:18:14 +0200

    nuevo front

--- src/main/java/andres/art_connect/ArtConnectApplication.java
+++ src/main/java/andres/art_connect/ArtConnectApplication.java
@@ -2,6 +2,8 @@ package andres.art_connect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @SpringBootApplication
@@ -10,5 +12,4 @@ public class ArtConnectApplication {
     public static void main(final String[] args) {
         SpringApplication.run(ArtConnectApplication.class, args);
     }
-
 }
