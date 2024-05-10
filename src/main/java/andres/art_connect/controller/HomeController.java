666ed4127e822e878192a6c70da181026deb0e0a
andres jesús lópez garcía <andreslopgar17@gmail.com>
2024-05-10 11:32:49 +0200

    version heroku

--- src/main/java/andres/art_connect/controller/HomeController.java
+++ src/main/java/andres/art_connect/controller/HomeController.java
@@ -2,16 +2,23 @@ package andres.art_connect.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String index() {
-        return "Hello World!";
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getIndex() {
+        return "index.html";
+    }
+
+    // Manejar todas las demás solicitudes y redirigirlas a index.html
+    @RequestMapping(value = "/{path:[^\\.]*}", method = RequestMethod.GET)
+    public String redirect() {
+        return "forward:/";
     }
 
 }
